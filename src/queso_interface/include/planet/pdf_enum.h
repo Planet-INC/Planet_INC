69ee5696e62b792fbff1d020e034bc4596af36b2
Sylvain Plessis <sylvain.plessis@gmail.com>
2014-02-21 14:58:30 -0600

    Files relevant for QUESO interface

---
+++ src/queso_interface/include/planet/pdf_enum.h
@@ -0,0 +1,26 @@
+//-----------------------------------------------------------------------bl-
+//--------------------------------------------------------------------------
+//-----------------------------------------------------------------------el-
+
+#ifndef PLANET_PDF_ENUM_H
+#define PLANET_PDF_ENUM_H
+
+namespace Planet
+{
+
+  namespace PDFName
+  {
+     enum PDFName{Norm = 0,
+                  NorT,
+                  Unif,
+                  LogN,
+                  LogU,
+                  Diri,
+                  DiUn,
+                  DiUT,
+                  DirG
+                  };
+  }
+}
+
+#endif
